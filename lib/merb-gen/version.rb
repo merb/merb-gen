22452a1dae4a4b77912f941008ed5c9bc4408c2d
Pavel Kunc <pavel.kunc@gmail.com>
2010-03-20 22:13:45 +0000

    [all] Dumping version 1.1.0

--- lib/merb-gen/version.rb
+++ lib/merb-gen/version.rb
@@ -1,7 +1,7 @@
 module Merb
   module Generators
 
-    VERSION = '1.1.0.rc1'.freeze
+    VERSION = '1.1.0'.freeze
 
     # Duplicated here (originally in merb_datamapper)
     # This is currently the easiest way to get at the
