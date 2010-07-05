1e9b7cd7253e24ac92d1c3dc2dcb0a52015d7120
Jonathan Stott <jonathan.stott@gmail.com>
2010-07-05 12:00:34 +0100

    Update generated stack for DM 1.0
    * Make it generate a dependency on DM 1.0
    * Just depend on the sqlite-adapter, not the lower level do adapter

--- lib/merb-gen/version.rb
+++ lib/merb-gen/version.rb
@@ -8,10 +8,10 @@ module Merb
     # datamapper version requirements, and still seems
     # better than specifying the datamapper version in
     # merb-core itself.
-    DM_VERSION_REQUIREMENT = '~> 0.10'.freeze
+    DM_VERSION_REQUIREMENT = '~> 1.0'.freeze
 
     # Necessary to supply builtin support for do_sqlite3
-    DO_VERSION_REQUIREMENT = '~> 0.10'.freeze
+    DO_VERSION_REQUIREMENT = '~> 1.0'.freeze
 
   end
 end
