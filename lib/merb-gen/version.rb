cbf004eddcbe6e0363ffba8a747752fb5bc8189b
Nicos Gollan <gtdev@spearhead.de>
2011-04-26 11:54:35 +0200

    Bump main gem versions to 1.2.0.pre1, update dependencies
    merb-gen now generates dependencies on:
    
      * DataMapper ~> 1.1
      * DataObjects ~> 0.10

--- lib/merb-gen/version.rb
+++ lib/merb-gen/version.rb
@@ -1,17 +1,17 @@
 module Merb
   module Generators
 
-    VERSION = '1.2.0'.freeze
+    VERSION = '1.2.0.pre1'.freeze
 
     # Duplicated here (originally in merb_datamapper)
     # This is currently the easiest way to get at the
     # datamapper version requirements, and still seems
     # better than specifying the datamapper version in
     # merb-core itself.
-    DM_VERSION_REQUIREMENT = '~> 1.0'.freeze
+    DM_VERSION_REQUIREMENT = '~> 1.1'.freeze
 
     # Necessary to supply builtin support for do_sqlite3
-    DO_VERSION_REQUIREMENT = '~> 1.0'.freeze
+    DO_VERSION_REQUIREMENT = '~> 0.10'.freeze
 
     # merb-auth has separate versioning scheme as well
     AUTH_VERSION_REQUIREMENT = '~> 1.0'.freeze
