de638e915b794acd667b6abcf0a43a3b0ada038d
Nicos Gollan <gtdev@spearhead.de>
2011-08-08 15:04:57 +0200

    [merb-gen] Do not overwrite given destination root
    Fixes specs.

--- lib/merb-gen/app_generator.rb
+++ lib/merb-gen/app_generator.rb
@@ -15,7 +15,7 @@ module Merb
 
         _args, _options, _config = args
         _config.merge!({:no_merb_load => true})
-        _config.merge!({:destination_root => File.expand_path(file_name(_args[0]))})
+        _config.merge!({:destination_root => File.expand_path(file_name(_args[0]))}) unless _config.has_key?(:destination_root)
 
         super(_args, _options, _config)
       end
